5acc74a0924ed7e9ffb0162910b0920fff705516
Hugo <167751232+HugoFrancalanci@users.noreply.github.com>
2025-03-19 13:55:53 -0400

    Delete SYNERGY/SYNERGIES YOSRA directory

--- SYNERGY/SYNERGIES YOSRA/npcorr.m
+++
@@ -1,6 +0,0 @@
-function C = npcorr(X)
-% non perturbed correlation
-% utilise npcov pour la covariance
-C = npcov(X);
-d_ = diag(C);
-C = C./sqrt(d_*d_');
--- SYNERGY/SYNERGIES YOSRA/offdiag.m
+++
@@ -1,6 +0,0 @@
-function M = offdiag(A)
-% return all the off - diagonal elements
-
-[M N] = size(A);
-idx = logical(triu((1-eye(M,N))));
-M = X(idx);
